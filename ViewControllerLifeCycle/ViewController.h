60ef197fe0e52b8f256bd6008fd922643f1e3adf
ssj <shishujuanhust@163.com>
2016-05-21 10:01:22 +0800

    Initial Commit

---
+++ ViewControllerLifeCycle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ViewControllerLifeCycle
+//
+//  Created by ssj on 16/5/21.
+//  Copyright © 2016年 ease. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ViewControllerLifeCycle/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ViewControllerLifeCycle
+//
+//  Created by ssj on 16/5/21.
+//  Copyright © 2016年 ease. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
