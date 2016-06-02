21b2671f0d106954812480dcdec03dcb23541fc6
ssj <shishujuanhust@163.com>
2016-06-02 18:04:27 +0800

    add lifecycle demo

---
+++ lifecycle/ViewControllerLifeCycle/ViewControllerLifeCycle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ViewControllerLifeCycle
+//
+//  Created by ssj on 16/5/21.
+//  Copyright © 2016年 ease. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
